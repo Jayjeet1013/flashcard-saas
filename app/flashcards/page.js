a1a7607c18771f7e7fe1b2ff477193744ab786ea
Jayjeet1013 <jayjeet1013@gmail.com>
2024-08-16 16:28:58 +0530

    solve bug

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -1,3 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+import { Container, TextField, Button,Grid,CardActionArea,Card,CardContent, Typography, Box } from "@mui/material";
+
 export default function Flashcard() {
   const { isLoaded, isSignedIn, user } = useUser();
   const [flashcards, setFlashcards] = useState([]);
@@ -18,28 +24,27 @@ export default function Flashcard() {
     getFlashcards();
   }, [user]);
 
+  const handleCardClick = (id) => {
+    router.push(`/flashcard?id=${id}`);
+  };
 
- const handleCardClick = (id) => {
-   router.push(`/flashcard?id=${id}`);
- };
-
-return (
-  <Container maxWidth="md">
-    <Grid container spacing={3} sx={{ mt: 4 }}>
-      {flashcards.map((flashcard, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <Card>
-            <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {flashcard.name}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
-  </Container>
-);
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3} sx={{ mt: 4 }}>
+        {flashcards.map((flashcard, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {flashcard.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
