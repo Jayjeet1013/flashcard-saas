aaf6f46fe8e147c580af4b02022680076aec8b0e
Jayjeet1013 <jayjeet1013@gmail.com>
2024-08-16 16:24:34 +0530

    adding animation to flashcards & ty to integrate firebase

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirebase} from 'firebase/firestore'
+import {getFirestore} from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirebase(app);
+const db = getFirestore(app);
 export {db};
