445970013217276d7672766b351c3e50a9d6f21c
Jayjeet1013 <jayjeet1013@gmail.com>
2024-12-14 13:45:54 +0530

    Fix: Fix bugs

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -9,7 +9,7 @@ const systemPrompt = (userMessage) => `
     Please provide the question and answer for the flashcard.
     Create only 10 flashcards
 
-    Return in the following JSON format:
+     Return the result strictly in JSON format, without any markdown, backticks, or additional formatting:
     {
         "flashcards": [{
             "front": "Question text here",
@@ -76,48 +76,5 @@ export async function POST(req) {
     );
   }
 }
-
-
-// using Openai
-
-// import { NextResponse } from "next/server";
-// import OpenAI from "openai";
-
-// const systemPrompt = `
-// You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
-// Both front and back should be one sentence long.
-// You should return in the following JSON format:
-// {
-//   "flashcards":[
-//     {
-//       "front": "Front of the card",
-//       "back": "Back of the card"
-//     }
-//   ]
-// }
-// `;
-
-
-// export async function POST(req) {
-//   const openai = new OpenAI();
-//   const data = await req.text();
-
- 
-//   const completion = await openai.chat.completions.create({
-//     messages: [
-//       { role: "system", content: systemPrompt },
-//       { role: "user", content: data },
-//     ],
-//     model: "gpt-4o",
-//     response_format: { type: "json_object" },
-//   });
-
-
-//     const flashcards = JSON.parse(completion.choices[0].message.content);
-
-//     // Return the flashcards as a JSON response
-//     return NextResponse.json(flashcards.flashcards);
-
-// }
 
 
